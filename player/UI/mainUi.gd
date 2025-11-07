5e5180010b95eb98de188c8a119fb2b3e69c6c07
NyanjaxX <166942889+zaydenstuff@users.noreply.github.com>
2025-11-08 00:06:32 +0100

    Update mainUi.gd

--- player/UI/mainUi.gd
+++ player/UI/mainUi.gd
@@ -70,6 +70,8 @@ func createIndexBrainrots():
 	await get_tree().process_frame
 	
 	print("creating index things")
+	if !Global.myPlrData: return
+	if !Global.myPlrData.find_key("indexBrainrots"): return
 	for i in Global.myPlrData["indexBrainrots"]:
 		print("index: ",i)
 		var ins = load("res://scenes/indexThing.tscn").instantiate()
